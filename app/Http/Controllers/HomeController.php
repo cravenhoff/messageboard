4d90bcb21e4bb664c94d2b874baa8aa48c5ce878
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-24 21:10:04 +1000

    Created new controller for the Home Page
    Added an index class and updated the web routes file.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    // Create an 'index' method that displays the default view of the home page
+    public function index() {
+        return view('welcome');
+    }
+}
