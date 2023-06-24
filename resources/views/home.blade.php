735e91cd4be49e958fb1dd08f237647116f102f5
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-24 22:56:07 +1000

    Added the 'post a message' form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,20 @@
 
 <!-- Main Content Section here -->
 @section('content')
+    <h5>Post a Message:</h5>
+    <form action="/create" method="post" class="mb-3">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title">
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Content</label>
+            <textarea name="content" id="content" cols="30" rows="5" class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
     <h5>Recent Messages:</h5>
     <ul>
         @foreach ($messages as $message)
