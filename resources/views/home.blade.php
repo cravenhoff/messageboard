6184a2b9fcbdaa853c00e9ab3f48e080fb81ee4a
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-24 21:46:32 +1000

    Created a master blade template file and a view for the home page
    Subsequently updated the controller to  point to the home page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!-- Name the file to extend -->
+@extends('master');
+
+<!-- Create the sections that yield blade template will reference -->
+@section('title', 'Home - Message Board')
+
+@section('content')
+    <h1>Home Page: Main Content</h1>
+@endsection
