57396400dda546ca53d6e36218a20544c748bc5c
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-24 23:32:52 +1000

    Updated the post listing to cards
    Added proper formatting and design to the post listing.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,28 +6,33 @@
 
 <!-- Main Content Section here -->
 @section('content')
-    <h5>Post a Message:</h5>
-    <form action="/create" method="post" class="mb-3">
-        @csrf
-        <div class="mb-3">
-            <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" name="title">
-        </div>
-        <div class="mb-3">
-            <label for="content" class="form-label">Content</label>
-            <textarea name="content" id="content" cols="30" rows="5" class="form-control"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div>
+        <h4>Post a Message:</h4>
+        <form action="/create" method="post" class="mb-3">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="content" class="form-label">Content</label>
+                <textarea name="content" id="content" cols="30" rows="5" class="form-control"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 
-    <h5>Recent Messages:</h5>
-    <ul>
+    <div>
+        <h4>Recent Messages:</h4>
         @foreach ($messages as $message)
-            <li class="mb-3">
-                <strong>{{ $message->title }}</strong><br> 
-                {{ $message->content }} <br>
-                {{ Carbon\Carbon::parse($message->created_at)->diffForHumans() }}
-            </li>
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $message->title }}</h5>
+                    <p class="card-text">{{ $message->content }}</p>
+                    <div class="fw-light text-muted"><small>{{ Carbon\Carbon::parse($message->created_at)->diffForHumans() }}</small></div>
+                </div>
+            </div>
         @endforeach
-    </ul>
+    </div>
+    
 @endsection
